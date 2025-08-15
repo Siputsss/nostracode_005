2fcf052f853a3a1cb3132ea91c99663eed072f96
siputsss <siput.apriliani@gmail.com>
2025-08-15 15:43:06 +0700

    build: add pages

--- lib/app/inits.dart
+++ lib/app/inits.dart
@@ -3,6 +3,9 @@ part of '_index.dart';
 Future<void> inits() async {
   logx.wtf('inits start');
 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   Serv.sample.init();
 
   logx.wtf('inits success');
