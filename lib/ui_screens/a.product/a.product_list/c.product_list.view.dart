74ff0b15e8a0b3febfc4cf9973265f2be969cb0e
siputsss <siput.apriliani@gmail.com>
2025-08-15 20:00:12 +0700

    updateDoc & readDoc

--- lib/ui_screens/a.product/a.product_list/c.product_list.view.dart
+++ lib/ui_screens/a.product/a.product_list/c.product_list.view.dart
@@ -29,9 +29,33 @@ class ProductListView extends StatelessWidget {
                         },
                         icon: const Icon(Icons.delete),
                       ),
+                      IconButton(
+                        onPressed: () {
+                          _ct.updateDoc(data[index]);
+                        },
+                        icon: const Icon(Icons.update),
+                      ),
                     ],
                   ),
+                  onTap: () => _ct.readDoc(data[index].id),
                 ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text('Product Detail'),
+            const SizedBox(height: 20),
+            OnBuilder.data(
+              listenTo: _dt.rxProductDetail,
+              builder: (data) => Column(
+                children: [
+                  Text('${data?.id}'),
+                  Text('${data?.brand}'),
+                  Text('${data?.model}'),
+                  Text('${data?.year}'),
+                  Text('${data?.price}'),
+                  Text('${data?.createdAt}'),
+                  Text('${data?.updatedAt}'),
+                ],
               ),
             ),
           ],
