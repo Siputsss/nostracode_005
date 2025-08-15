f47994c5cdb56fcb46b89c0da528e9c07383840f
siputsss <siput.apriliani@gmail.com>
2025-08-15 15:43:06 +0700

    build: add pages

--- lib/app/navigation/c.route_map.dart
+++ lib/app/navigation/c.route_map.dart
@@ -8,5 +8,9 @@ class RouteMap {
     Routes.sampleSatu: (RouteData data) => const SampleSatuView(),
     Routes.sampleDua: (RouteData data) => const SampleDuaView(),
     Routes.sampleTiga: (RouteData data) => const SampleTigaView(),
+    Routes.productList: (RouteData data) => const ProductListView(),
+    Routes.productDetail: (RouteData data) => const ProductDetailView(),
+    Routes.productAdd: (RouteData data) => const ProductAddView(),
+    Routes.productEdit: (RouteData data) => const ProductEditView(),
   };
 }
--- lib/ui_screens/_index.dart
+++ lib/ui_screens/_index.dart
@@ -5,3 +5,8 @@ export 'x.sample/a.sample_satu/_index.dart';
 export 'x.sample/b.sample_dua/_index.dart';
 
 export 'x.sample/c.sample_tiga/_index.dart';
+
+export 'a.product/a.product_list/_index.dart';
+export 'a.product/b.product_detail/_index.dart';
+export 'a.product/c.product_add/_index.dart';
+export 'a.product/d.product_edit/_index.dart';
